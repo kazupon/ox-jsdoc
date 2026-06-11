f45d5edec6d08bc912ba3ef83dfb7177a1b6fb9b
kazuya kawaguchi <kawakazu80@gmail.com>
2026-06-11 19:08:09 +0900

    fix: setup void

--- apps/playground/vite.config.ts
+++ apps/playground/vite.config.ts
@@ -1,6 +1,7 @@
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
+import { voidPlugin } from 'void'
 
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [voidPlugin(), vue()]
 })
